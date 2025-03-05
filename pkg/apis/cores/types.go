d9a3a82027d5b4d58723b3b9f8624d0692bb58f3
microsurge <1172798125@qq.com>
2025-03-05 20:16:59 +0800

    3.5 score plugins pt1

--- pkg/apis/cores/types.go
+++ pkg/apis/cores/types.go
@@ -592,6 +592,8 @@ type GroupSpec struct {
 	// Group中的Action需要有较严格的依赖顺序，可以支持分支,条件,循环
 	// 只能有一个Action作为入口Action,图形结构
 
+	ResourceRequirements []ResourceRequirement `json:"resource_requirements,omitempty" yaml:"resource_requirements"`
+
 	SkipScorePlugins []string `json:"skip_score_plugins,omitempty" yaml:"skip_score_plugins"`
 
 	SkipFilterPlugins []string `json:"skip_filter_plugins,omitempty" yaml:"skip_filter_plugins"`
@@ -601,6 +603,12 @@ type GroupSpec struct {
 
 	//-临时添加-k8s运行时相关，还未重构，后期会重构
 	Labels map[string]string // 添加 Labels 字段，用于选择器
+}
+
+type ResourceRequirement struct {
+	Name       string `json:"name,omitempty" yaml:"name"`
+	Lowbound   string `json:"lowbound,omitempty" yaml:"lowbound"`
+	Upperbound string `json:"upperbound,omitempty" yaml:"upperbound"`
 }
 
 type GroupStatus struct {
--- pkg/scheduler/framework/plugins/score_dby.go
+++ pkg/scheduler/framework/plugins/score_dby.go
@@ -6,10 +6,15 @@ import (
 	"hit.edu/framework/pkg/scheduler/framework"
 	"hit.edu/framework/pkg/scheduler/transport"
 	"hit.edu/framework/pkg/scheduler/transport/client"
+	"net/http"
 )
 
 type ScorePluginDBY struct {
-	SchedClient transport.SchedulerClient
+	pluginClient ScorePluginClient
+}
+
+type ScorePluginClient struct {
+	client *http.Client
 }
 
 func (sp *ScorePluginDBY) Name() string {
@@ -22,7 +27,18 @@ func (sp *ScorePluginDBY) Score(ctx context.Context, group *apis.Group, nodeName
 		Group:  group,
 		NodeID: nodeName,
 	}
-	resp := sp.SchedClient.SendScoreRequest(request)
+	resp := sp.SendScoreRequest(request)
+	if resp.BaseResp.Success {
+		return resp.Data.Score, framework.NewStatus(framework.Success, "default success")
+	}
+	return -1, framework.NewStatus(framework.Error, resp.BaseResp.Reason)
+}
+
+func (sp *ScorePluginDBY) SendGroups(ctx context.Context, task *apis.Task) (int64, *framework.Status) {
+	//TODO 没测过
+
+	request := buildSendGroupsRequest(ctx, task)
+	resp := sp.SendScoreRequest(request)
 	if resp.BaseResp.Success {
 		return resp.Data.Score, framework.NewStatus(framework.Success, "default success")
 	}
@@ -31,6 +47,50 @@ func (sp *ScorePluginDBY) Score(ctx context.Context, group *apis.Group, nodeName
 
 func NewScorePluginDBY(ctx context.Context, f framework.Handle) (framework.Plugin, error) {
 	return &ScorePluginDBY{
-		SchedClient: client.GetHClient(),
+		SchedulerClient: client.GetHClient(),
 	}, nil
+}
+
+type GroupTopInfo struct {
+	Parent string `json:"parent"`
+	Child  string `json:"child"`
+}
+
+type SendGroupsRequest struct {
+	Groups              []string                              `json:"groups"`
+	TaskId              string                                `json:"taskId"`
+	ResourceRequirement map[string][]apis.ResourceRequirement `json:"resourceRequirement"`
+	TopInfo             []GroupTopInfo                        `json:"topInfo"`
+}
+
+func buildSendGroupsRequest(ctx context.Context, task *apis.Task) *SendGroupsRequest {
+	topInfo := make([]GroupTopInfo, 0)
+	groupsID := make([]string, 0)
+	resourcesMap := make(map[string][]apis.ResourceRequirement)
+	taskID := task.UID
+	//TODO 可能需要做深复制 @lbh
+	for _, group := range task.Spec.Groups {
+		topInfo = append(topInfo, GroupTopInfo{})
+		groupsID = append(groupsID, string(group.ObjectMeta.UID))
+		resources := make([]apis.ResourceRequirement, 0)
+		for _, requirement := range group.Spec.ResourceRequirements {
+			resources = append(resources, requirement)
+		}
+		if len(resources) > 0 {
+			resourcesMap[string(group.ObjectMeta.UID)] = resources
+		}
+		for _, parent := range group.Spec.Parents {
+			topInfo = append(topInfo, GroupTopInfo{
+				Child: string(group.ObjectMeta.UID),
+				//TODO 这个里面放的估计不是uid。。
+				Parent: parent,
+			})
+		}
+	}
+	return &SendGroupsRequest{
+		Groups:              groupsID,
+		TaskId:              string(taskID),
+		ResourceRequirement: resourcesMap,
+		TopInfo:             topInfo,
+	}
 }
