cfac1916e647dfc4682bd05c1c8645a040d2a631
linbohai <1172798125@qq.com>
2025-03-13 11:10:10 +0000

    save

--- pkg/apis/cores/types.go
+++ pkg/apis/cores/types.go
@@ -603,6 +603,9 @@ type GroupSpec struct {
 
 	//-临时添加-k8s运行时相关，还未重构，后期会重构
 	Labels map[string]string // 添加 Labels 字段，用于选择器
+
+	//亲和节点，如果该字段不为空的话，那么group就必须放在这些节点上执行
+	AffinityNodes []string `json:"affinity_nodes,omitempty" yaml:"affinity_nodes"`
 }
 
 type ResourceRequirement struct {
