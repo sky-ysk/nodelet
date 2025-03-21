6f95b39c84b4bbc36dbe3fb002c1f0cc723a41d0
microsurge <1172798125@qq.com>
2025-03-21 14:07:56 +0800

    save

--- pkg/nodelet/task/runtime/device/ability/lib/predict_test.go
+++ pkg/nodelet/task/runtime/device/ability/lib/predict_test.go
@@ -1,18 +1,30 @@
 package lib
 
 import (
+	"fmt"
 	"hit.edu/framework/pkg/component-base/logs"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
 func TestPublishPredictInst(t *testing.T) {
 	logs.Init("test")
-	imagePath := ""
-	url := "http://127.0.0.1:8123"
+
+	// 获取当前测试文件的目录
+	testDir, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("无法获取当前工作目录: %v", err)
+	}
+
+	// 构建相对路径
+	imagePath := filepath.Join(testDir, "testpics", "orange.jpg")
+	url := "http://192.168.8.165:48479"
 	resp, err := PublishPredictInst(imagePath, url)
 	if err != nil {
 		logs.Errorf("error publish predict inst %v", err)
 	} else {
 		logs.Infof("predict result is %v", resp)
+		fmt.Println(resp)
 	}
 }
