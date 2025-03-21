6099283bb8c8e416fb5d73b7f0b8e745ac4623a1
sky <2608462408@qq.com>
2025-03-20 21:47:03 -0700

    修改测试案例

--- pkg/nodelet/task/runtime/device/ability/lib/arm_angle_test.go
+++ pkg/nodelet/task/runtime/device/ability/lib/arm_angle_test.go
@@ -1,18 +1,39 @@
 package lib
 
 import (
+	"fmt"
 	"hit.edu/framework/pkg/component-base/logs"
+	"hit.edu/framework/pkg/nodelet/task/runtime/device/ability/manager"
+	"strconv"
 	"testing"
 )
 
 func TestPublishArmAngleInst(t *testing.T) {
 	logs.Init("test")
-	var params map[string][]float64 = make(map[string][]float64)
-	params["left"] = []float64{-1.221, 0.0872, 0, 0, 0, 0, 0}
-	params["right"] = []float64{0, 0, 0, 0, 0, 0, 0}
-	url := ""
-	err := PublishArmAngleInst(params, url)
+	managerUrl := ""
+	abilityName := ""
+	am := manager.NewAbilityManager(managerUrl, abilityName)
+	heartBeat, err := am.StartupAbility()
+
+	//// 终止能力[备用]
+	//err =am.TerminateAbility()
+	//if err!=nil{
+	//	logs.Errorf("error is %v",err)
+	//}
 	if err != nil {
-		logs.Errorf("error publish ArmAngle inst %v", err)
+		logs.Errorf("start up %s fail", abilityName)
+	} else {
+		var params map[string][]float64 = make(map[string][]float64)
+		params["left"] = []float64{-1.221, 0.0872, 0, 0, 0, 0, 0}
+		params["right"] = []float64{0, 0, 0, 0, 0, 0, 0}
+
+		// 通过heartbeat中的abilityPort拼接成新的url
+		url := ""
+		serviceUrl := fmt.Sprintf("%s:%s", url, strconv.Itoa(heartBeat.AbilityPort))
+		err := PublishArmAngleInst(params, serviceUrl)
+		if err != nil {
+			logs.Errorf("error publish ArmAngle inst %v", err)
+		}
 	}
+
 }
--- pkg/nodelet/task/runtime/device/ability/lib/predict_test.go
+++ pkg/nodelet/task/runtime/device/ability/lib/predict_test.go
@@ -1,18 +1,40 @@
 package lib
 
 import (
+	"fmt"
 	"hit.edu/framework/pkg/component-base/logs"
+	"hit.edu/framework/pkg/nodelet/task/runtime/device/ability/manager"
+	"strconv"
 	"testing"
 )
 
 func TestPublishPredictInst(t *testing.T) {
 	logs.Init("test")
-	imagePath := ""
-	url := "http://127.0.0.1:8123"
-	resp, err := PublishPredictInst(imagePath, url)
+	managerUrl := ""
+	abilityName := ""
+	am := manager.NewAbilityManager(managerUrl, abilityName)
+	heartBeat, err := am.StartupAbility()
+
+	//// 终止能力[备用]
+	//err =am.TerminateAbility()
+	//if err!=nil{
+	//	logs.Errorf("error is %v",err)
+	//}
 	if err != nil {
-		logs.Errorf("error publish predict inst %v", err)
+		logs.Errorf("start up %s fail", abilityName)
 	} else {
-		logs.Infof("predict result is %v", resp)
+		imagePath := ""
+
+		// 通过heartbeat中的abilityPort拼接成新的url
+		url := ""
+		serviceUrl := fmt.Sprintf("%s:%s", url, strconv.Itoa(heartBeat.AbilityPort))
+		resp, err := PublishPredictInst(imagePath, serviceUrl)
+		if err != nil {
+			logs.Errorf("error publish ArmAngle inst %v", err)
+		} else {
+			logs.Infof("predict resp is %v", resp)
+		}
+
 	}
+
 }
