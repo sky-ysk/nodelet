370d8cead8ae483f3bccc0e43635944cd4c07074
microsurge <1172798125@qq.com>
2025-03-25 11:42:07 +0800

    condition pt3

--- runScheduler.sh
+++ runScheduler.sh
@@ -55,6 +55,7 @@ if [ -f "$NODELET_PATH" ]; then
       echo "The nodelet is already running. Restarting nodelet ..."
       killall nodelet
   fi
+  echo "run the nodelet"
     "$NODELET_PATH"
 else
     echo "The nodelet file at $NODELET_PATH does not exist."
