e88bc0b0fab621163600183a582fb89569bc8743
microsurge <1172798125@qq.com>
2025-03-31 18:30:05 +0800

    solve

--- pkg/scheduler/transport/types.go
+++ pkg/scheduler/transport/types.go
@@ -1,14 +1,10 @@
 package transport
 
-import (
-	apis "hit.edu/framework/pkg/apis/cores"
-)
-
 type ScoreRequest struct {
-	TaskID string `json:"task_id"`
+	TaskID     string `json:"task_id"`
 	PipelineId string `json:"pipeline_id"`
 	SecID      string `json:"sec_id"`
-	GroupID string `json:"group_id"`
+	GroupID    string `json:"group_id"`
 	//Group      *apis.Group `json:"group"`
 	NodeID string `json:"nodeId"`
 	Host   string `json:"host"`
