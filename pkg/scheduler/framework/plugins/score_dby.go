e67cc98c6f3195a437d63cdf37b1203f3fcf210e
microsurge <1172798125@qq.com>
2025-04-01 21:33:09 +0800

    dts test 2

--- pkg/scheduler/framework/plugins/score_dby.go
+++ pkg/scheduler/framework/plugins/score_dby.go
@@ -140,7 +140,7 @@ func (sp *ScorePluginDBY) Score(ctx context.Context, group *apis.Group, nodeName
 	fmt.Println("request is")
 	fmt.Println(string(jsonData))
 	data, err := sp.pluginClient.SendData(jsonData, "/schedule/getSchedule")
-	time.Sleep(1 * time.Second)
+	time.Sleep(5 * time.Second)
 	data, err = sp.pluginClient.SendData(jsonData, "/schedule/getSchedule")
 	if err != nil {
 		return 0, framework.NewStatus(framework.Error, err.Error())
