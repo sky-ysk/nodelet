344a99d0c0f179982bfa3e5973cf361eeb2b33da
or1ando <or1ando@163.com>
2025-04-20 17:30:57 +0800

    CheckDevice初步实现

--- pkg/nodelet/task/runtime/device/utils/device.go
+++ pkg/nodelet/task/runtime/device/utils/device.go
@@ -11,6 +11,7 @@ import (
 )
 
 func CheckDevices(deviceMap map[string]*apis.Device, specs []apis.DeviceSpec) error {
+	// 遍历device
 	for name, device := range deviceMap {
 		logs.Infof("[DEVICE RUNTIME] Check Device[%s] ", name)
 		// 检查device的GroupID phase
@@ -18,18 +19,25 @@ func CheckDevices(deviceMap map[string]*apis.Device, specs []apis.DeviceSpec) er
 			logs.Errorf("[DEVICE RUNTIME] Device[%s] is not IDLE", name)
 			return fmt.Errorf("error! Device[%s] is not IDLE", name)
 		}
+		logs.Infof("[DEVICE RUNTIME] Check Device[%s] Phase is NORMAL", device.Name)
 		// 检查是否上锁
 		if device.Status.Lock.IsLocked != true {
 			logs.Errorf("[DEVICE RUNTIME] Device[%s] is not locked", name)
 			return fmt.Errorf("error! Device[%s] is not locked", name)
 		}
+		logs.Infof("[DEVICE RUNTIME] Check Device[%s] Lock is NORMAL", name)
 		// 检查能力的状态
 		for _, spec := range specs {
 			for _, abilityName := range spec.Abilities {
 				if ability, ok := device.Status.Abilities[abilityName]; ok {
-					state := apis.AbilityRunning
-					if ability.State != state {
+					if ability.Status != apis.AbilityRunning { // 做一些处理
+						logs.Warnf("[DEVICE RUNTIME] Device[%s] is not running]", name)
+						if ability.Status == apis.AbilityReadyStartUp {
+							logs.Warnf("[DEVICE RUNTIME] Device[%s] is ready STARTUP", name)
+						}
 
+					} else {
+						logs.Infof("[DEVICE RUNTIME] Device[%s] Ability[%s] is NORMAL", name, abilityName)
 					}
 				}
 			}
@@ -37,6 +45,7 @@ func CheckDevices(deviceMap map[string]*apis.Device, specs []apis.DeviceSpec) er
 		}
 
 	}
+	return nil
 }
 
 // UpdateDeviceRunning 用于在发布任务指令成功后(但是还不知道业务执行情况)时 更新device的状态
