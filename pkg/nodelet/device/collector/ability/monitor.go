798b9d523dd94612b94cded30e55234bffd5d115
microsurge <1172798125@qq.com>
2025-04-24 16:30:43 +0800

    save 5

--- pkg/nodelet/device/collector/ability/monitor.go
+++ pkg/nodelet/device/collector/ability/monitor.go
@@ -216,7 +216,6 @@ func MonitorAllAbilities(clientManager *m.Manager) error {
 		// Ability类型的device
 		if device.Spec.AccessMethod.Type == apis.AccessByAbility {
 			wg.Add(1)
-
 			// 每个Device单独开一个协程
 			go func() {
 				defer wg.Done()
@@ -227,30 +226,28 @@ func MonitorAllAbilities(clientManager *m.Manager) error {
 						logs.Infof("[DEVICE EXPORTER-ABILITY MONITOR] Monitor Device[%s] Ability[%s]", device.Name, ability.Name)
 						if ability.Status == apis.AbilityReadyStartUp { // 如果ability需要被拉起就给他拉起
 							logs.Infof("[DEVICE EXPORTER-ABILITY MONITOR] Device[%s] Ability[%s] is ReadyStartUp", device.Name, ability.Name)
-							go func() {
-								am := NewAbilityManager(url, ability.Name)
-								err = am.BindUUID()
-								if err != nil {
-									logs.Errorf("[DEVICE EXPORTER-ABILITY MONITOR] Device[%s] Ability[%s] Bind uuid failed, err:%s", device.Name, ability.Name, err.Error())
-									errChan <- err
-									return
-								}
-								var hb HeartBeat
-								hb, err = am.StartupAbility()
-								if err != nil {
-									logs.Errorf("[DEVICE EXPORTER-ABILITY MONITOR] Device[%s] Ability[%s] Startup failed, err:%s", device.Name, ability.Name, err.Error())
-									errChan <- err
-									return
-								}
-								logs.Infof("[DEVICE EXPORTER-ABILITY MONITOR] Device[%s] Ability[%s] Startup Success!", device.Name, ability.Name)
-								ability.Status = apis.AbilityRunning
-								for sn, service := range ability.Services {
-									port := strconv.Itoa(hb.AbilityPort)
-									service.Port = &port
-									ability.Services[sn] = service
-								}
-								device.Status.Abilities[name] = ability
-							}()
+							am := NewAbilityManager(url, ability.Name)
+							err = am.BindUUID()
+							if err != nil {
+								logs.Errorf("[DEVICE EXPORTER-ABILITY MONITOR] Device[%s] Ability[%s] Bind uuid failed, err:%s", device.Name, ability.Name, err.Error())
+								errChan <- err
+								return
+							}
+							var hb HeartBeat
+							hb, err = am.StartupAbility()
+							if err != nil {
+								logs.Errorf("[DEVICE EXPORTER-ABILITY MONITOR] Device[%s] Ability[%s] Startup failed, err:%s", device.Name, ability.Name, err.Error())
+								errChan <- err
+								return
+							}
+							logs.Infof("[DEVICE EXPORTER-ABILITY MONITOR] Device[%s] Ability[%s] Startup Success!", device.Name, ability.Name)
+							ability.Status = apis.AbilityRunning
+							for sn, service := range ability.Services {
+								port := strconv.Itoa(hb.AbilityPort)
+								service.Port = &port
+								ability.Services[sn] = service
+							}
+							device.Status.Abilities[name] = ability
 
 						}
 					}
