506d09963170bfb78baf228ad6928a1813721cbe
和小白 <10233011+and-xiaobai@user.noreply.gitee.com>
2025-04-27 17:15:51 +0800

    提交一些Demo和修改日志

--- pkg/nodelet/task/runtime/command/command.go
+++ pkg/nodelet/task/runtime/command/command.go
@@ -110,7 +110,7 @@ func (cr *CommandRuntime) startCMD(groupName, groupNamespace string, actionSpeNa
 	//CMD.Stdout = outfile
 	CMD.Stderr = os.Stderr
 	// CMD.Env = append(CMD.Env, )
-	CMD.Dir = apis.FileFolder + runtime.Spec.Name // 设置工作目录,TODO：注意需要在拉起runtime的时候创建好文件夹
+	//CMD.Dir = apis.FileFolder + runtime.Spec.Name // 设置工作目录,TODO：注意需要在拉起runtime的时候创建好文件夹
 	// 启动命令
 	logs.Infof("runtime Name:\t %s is Running", runtime.Name)
 
