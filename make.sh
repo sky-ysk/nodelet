7d99f759ef70d75d9c02d93ebe6e7622db904a06
microsurge <1172798125@qq.com>
2025-05-14 14:04:02 +0800

    device bingding plugin pt 4

--- make.sh
+++ make.sh
@@ -5,4 +5,4 @@ make all WHAT=./cmd/proxy FRAMEWORK_VERBOSE=3
 make all WHAT=./cmd/apiserver FRAMEWORK_VERBOSE=3
 make all WHAT=./cmd/nodelet FRAMEWORK_VERBOSE=3
 make all WHAT=./cmd/scheduler FRAMEWORK_VERBOSE=3
-make all WHAT=./cmd/registry FRAMEWORK_VERBOSE=3
+#make all WHAT=./cmd/registry FRAMEWORK_VERBOSE=3
