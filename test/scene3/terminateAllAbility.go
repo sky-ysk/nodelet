32324587a0d4744fca0c134a9c7a828dc8ae53e3
or1ando@163.com <or1ando@163.com>
2025-05-17 05:35:37 +0000

    save

--- test/scene3/terminateAllAbility.go
+++ test/scene3/terminateAllAbility.go
@@ -10,14 +10,26 @@ func main() {
 	logs.Init(moduleName)
 	logs.Infof("[test] testing run.....\n")
 	var url1 string = "http://172.130.0.61:8080" // 更改ip和端口
-	var name1 string = "GrabObject.Leju.801"     // 更改能力名字
-	var name2 string = "PutObject.Leju.801"      // 更改能力名字
-	var name3 string = "Turn.Leju.801"           // 更改能力名字
-	var name4 string = "DetectObject.Leju.801"   // 更改能力名字
+	var url2 string = "http://172.130.0.59:8080"
+	var name1 string = "GrabObject.Leju.801"   // 更改能力名字
+	var name2 string = "PutObject.Leju.801"    // 更改能力名字
+	var name3 string = "Turn.Leju.801"         // 更改能力名字
+	var name4 string = "DetectObject.Leju.801" // 更改能力名字
 
 	nameList := []string{name1, name2, name3, name4}
 	for _, name := range nameList {
 		abilityManager := manager.NewAbilityManager(url1, name)
+		err := abilityManager.BindUUID()
+		if err != nil {
+			logs.Errorf("bind uuid fail")
+		}
+		err = abilityManager.TerminateAbility()
+		if err != nil {
+			logs.Errorf("terminate ability fail")
+		}
+	}
+	for _, name := range nameList {
+		abilityManager := manager.NewAbilityManager(url2, name)
 		err := abilityManager.BindUUID()
 		if err != nil {
 			logs.Errorf("bind uuid fail")
