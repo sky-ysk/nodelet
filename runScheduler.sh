5e6d3689b98e6311227f86401820a345e383be64
or1ando <or1ando@163.com>
2025-05-19 13:56:54 +0800

    save

--- runScheduler.sh
+++ runScheduler.sh
@@ -35,7 +35,6 @@ check_process_running() {
 #
 #echo "The apiserver output is redirected to apiserver_log.log, and the scheduler output is shown in the foreground."
 
-
 # 休眠 2 秒
 sleep 2
 
@@ -52,8 +51,7 @@ else
     exit 1
 fi
 
-echo "The proxy output is redirected to proxy_log.log, and the scheduler output is shown in the foreground."
-
+echo "The proxy output is redirected to proxy_log.log."
 
 # 休眠 2 秒
 sleep 2
@@ -64,25 +62,27 @@ if [ -f "$SCHEDULER_PATH" ]; then
       echo "The scheduler is already running. Restarting scheduler ..."
       killall scheduler
   fi
-    "$SCHEDULER_PATH"
+  # 使用 nohup 将 scheduler 放到后台运行，并将输出重定向到 scheduler_log.log 文件
+  nohup "$SCHEDULER_PATH" > scheduler_log.log 2>&1 &
+  echo "The scheduler output is redirected to scheduler_log.log."
 else
     echo "The scheduler file at $SCHEDULER_PATH does not exist."
     exit 1
 fi
 
-
 # 休眠 2 秒
 sleep 2
 
 # 判断 nodelet 文件是否存在
-#if [ -f "$NODELET_PATH" ]; then
-#  if check_process_running "$NODELET_PATH"; then
-#      echo "The nodelet is already running. Restarting nodelet ..."
-#      killall nodelet
-#  fi
-#  echo "run the nodelet"
-#    "$NODELET_PATH"
-#else
-#    echo "The nodelet file at $NODELET_PATH does not exist."
-#    exit 1
-#fi
+if [ -f "$NODELET_PATH" ]; then
+  if check_process_running "$NODELET_PATH"; then
+      echo "The nodelet is already running. Restarting nodelet ..."
+      killall nodelet
+  fi
+  # 使用 nohup 将 nodelet 放到后台运行，并将输出重定向到 nodelet_log.log 文件
+  nohup "$NODELET_PATH" > nodelet_log.log 2>&1 &
+  echo "The nodelet output is redirected to nodelet_log.log."
+else
+    echo "The nodelet file at $NODELET_PATH does not exist."
+    exit 1
+fi
