fba6432cf5aad855da6145d63434c341ae1468d8
or1ando <or1ando@163.com>
2025-05-27 10:33:02 +0800

    注释前端演示页面特判逻辑

--- pkg/scheduler/schedule_one.go
+++ pkg/scheduler/schedule_one.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"math/rand"
 	"net/http"
-	"strings"
 	"time"
 
 	"hit.edu/framework/pkg/apimachinery/runtime"
@@ -277,13 +276,13 @@ func (sched *Scheduler) scheduleGroup(ctx context.Context,
 	}
 
 	//前端演示页面特判逻辑
-	if group.Spec.Desc != nil && len(group.Spec.Desc.Label) != 0 {
-		if strings.Contains(group.Spec.Desc.Label[0], "Infer") {
-			host = "EdgeNode1"
-		} else {
-			host = "CloudNode1"
-		}
-	}
+	//if group.Spec.Desc != nil && len(group.Spec.Desc.Label) != 0 {
+	//	if strings.Contains(group.Spec.Desc.Label[0], "Infer") {
+	//		host = "EdgeNode1"
+	//	} else {
+	//		host = "CloudNode1"
+	//	}
+	//}
 	//if strings.Contains(group.ObjectMeta.Name, "Train") {
 	//	host = "CloudNode1"
 	//}
